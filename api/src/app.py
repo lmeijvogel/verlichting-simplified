0737fc6672ee445b5f9d17cf6ed5af17f40140e2
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2023-07-13 22:24:20 +0200

    Only return known scenes and allow activation for known scenes.

--- api/src/app.py
+++ api/src/app.py
@@ -14,7 +14,9 @@ app = Flask(__name__)
 API_HOST = os.getenv("API_HOST")
 API_TOKEN = os.getenv("API_TOKEN")
 
-@app.route("/api/scenes", methods=['GET', 'POST'])
+ALLOWED_SCENES = ["scene.uit", "scene.ochtend", "scene.middag", "scene.avond", "scene.nacht"];
+
+@app.route("/api/scenes", methods=['GET'])
 def scenes():
     url = "http://%s/api/states" % API_HOST
 
@@ -25,10 +27,28 @@ def scenes():
 
     entities = json.loads(content)
 
+    scenes = [scene_to_json(entity) for entity in entities if entity["entity_id"] in ALLOWED_SCENES]
     return {
-            "scenes": [scene_to_json(entity) for entity in entities if (entity["entity_id"].startswith('scene.'))],
+            "scenes": scenes,
             "fullResponse": entities
     }
+
+@app.route("/api/start_scene/<scene_name>", methods=['POST'])
+def start_scene(scene_name):
+    if (scene_name in ALLOWED_SCENES):
+        url = "http://%s/api/services/scene/turn_on" % API_HOST
+
+        headers = {"Authorization": "Bearer %s" % API_TOKEN}
+
+        data = {"entity_id": scene_name }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        updated_scene = json.loads(response.content)[0]
+
+        return scene_to_json(updated_scene)
+    else:
+        return "Scene not found", 404
 
 def scene_to_json(scene):
     return {
